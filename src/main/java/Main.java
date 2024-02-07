74104f4f8b25d3ac6abf922178fa069e6b51d429
Credible-Zebra <92882620+Credible-Zebra@users.noreply.github.com>
2024-02-07 03:02:45 +0000

    Hello World program

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+// create main class
+public class Main {
+  // create main function
+  public static void main(String[] args) {
+    // print Hello World! to the console
+    System.out.println("Hello world!");
+  }
+}
